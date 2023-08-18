727b41569db455442ac0a07ee3d1facb1a2d451a
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-08-18 15:25:35 +0530

    Created Account Model

---
+++ src/CrimeWatch.Domain/Models/AccountModel/Account.cs
@@ -0,0 +1,16 @@
+﻿namespace CrimeWatch.Domain.Models.AccountModel;
+public class Account : AggregateRoot<AccountId>
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public bool IsModerator { get; set; } = false;
+
+    public static Account Create(string email, string password, bool isModerator)
+    {
+        return new()
+        {
+            Email = email,
+            Password = password,
+        };
+    }
+}
