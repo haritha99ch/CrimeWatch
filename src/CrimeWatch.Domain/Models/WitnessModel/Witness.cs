c2ebb0ea4a42c4141569d0dea8566b97f5db8fa3
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-08-18 15:40:20 +0530

    Created Witness Model

---
+++ src/CrimeWatch.Domain/Models/WitnessModel/Witness.cs
@@ -0,0 +1,33 @@
+﻿using CrimeWatch.Domain.Models.AccountModel;
+using CrimeWatch.Domain.Models.UserModel;
+
+namespace CrimeWatch.Domain.Models.WitnessModel;
+public class Witness : AggregateRoot<WitnessId>
+{
+    public UserId UserId { get; set; } = default!;
+    public AccountId AccountId { get; set; } = default!;
+
+    public Account? Account { get; set; }
+    public User? User { get; set; }
+
+    public static Witness Create(
+        string firstName,
+        string lastName,
+        Gender gender,
+        DateOnly dateOfBirth,
+        string phoneNumber,
+        string email,
+        string password
+        )
+    {
+        User user = User.Create(firstName, lastName, gender, dateOfBirth, phoneNumber);
+        Account account = Account.Create(email, password, false);
+
+        return new()
+        {
+            Id = new(new()),
+            User = user,
+            Account = account
+        };
+    }
+}
