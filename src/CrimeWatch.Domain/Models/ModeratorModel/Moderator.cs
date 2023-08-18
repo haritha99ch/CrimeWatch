78563f6aa0709f43d3cc79e084fd3f4a9f31d301
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-08-18 21:08:12 +0530

    Created Moderator Model

---
+++ src/CrimeWatch.Domain/Models/ModeratorModel/Moderator.cs
@@ -0,0 +1,37 @@
+﻿using CrimeWatch.Domain.Models.AccountModel;
+using CrimeWatch.Domain.Models.UserModel;
+
+namespace CrimeWatch.Domain.Models.ModeratorModel;
+public class Moderator : AggregateRoot<ModeratorId>
+{
+    public string PoliceId { get; set; } = string.Empty;
+    public UserId UserId { get; set; } = default!;
+    public AccountId AccountId { get; set; } = default!;
+    public string Province { get; set; } = string.Empty;
+
+    public User? User { get; set; }
+    public Account? Account { get; set; }
+
+    public static Moderator Create(string firstName,
+        string lastName,
+        Gender gender,
+        DateOnly dateOfBirth,
+        string phoneNumber,
+        string policeId,
+        string province,
+        string email,
+        string password)
+    {
+        User user = User.Create(firstName, lastName, gender, dateOfBirth, phoneNumber);
+        Account account = Account.Create(email, password, true);
+
+        return new Moderator
+        {
+            Id = new(new()),
+            User = user,
+            Account = account,
+            PoliceId = policeId,
+            Province = province
+        };
+    }
+}
