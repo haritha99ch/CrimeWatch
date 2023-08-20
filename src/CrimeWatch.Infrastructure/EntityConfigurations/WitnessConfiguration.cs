48292f5e688fe8ad88a52a1b7ac6dd3719ea5310
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-08-21 00:51:30 +0530

    Configured Witness Entity

---
+++ src/CrimeWatch.Infrastructure/EntityConfigurations/WitnessConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace CrimeWatch.Infrastructure.EntityConfigurations;
+internal class WitnessConfiguration : IEntityTypeConfiguration<Witness>
+{
+    public void Configure(EntityTypeBuilder<Witness> builder)
+    {
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.Id).HasConversion(e => e.Value, value => new(value));
+
+        builder.HasOne(e => e.Account).WithOne().HasForeignKey<Witness>(w => w.AccountId);
+
+        builder.HasOne(e => e.User).WithOne().HasForeignKey<Witness>(w => w.UserId);
+    }
+}
