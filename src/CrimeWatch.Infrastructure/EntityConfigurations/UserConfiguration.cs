fbd914f924604dbb52342b560128d7ddbc69692e
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-08-21 00:27:44 +0530

    Configured User Entity

---
+++ src/CrimeWatch.Infrastructure/EntityConfigurations/UserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace CrimeWatch.Infrastructure.EntityConfigurations;
+internal class UserConfiguration : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.Id).HasConversion(e => e.Value, value => new(value));
+
+        builder.Property(e => e.FirstName).HasMaxLength(50).IsRequired();
+
+        builder.Property(e => e.LastName).HasMaxLength(50).IsRequired();
+
+        builder.Property(e => e.DateOfBirth).IsRequired();
+
+        builder.Property(e => e.PhoneNumber).HasMaxLength(20).IsRequired();
+    }
+}
