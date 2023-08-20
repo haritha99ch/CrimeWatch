14876f7bfe65d6f036ab22610ed51cb131391138
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-08-21 00:41:16 +0530

    Configured MediaItem Entity

---
+++ src/CrimeWatch.Infrastructure/EntityConfigurations/MediaItemConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace CrimeWatch.Infrastructure.EntityConfigurations;
+internal class MediaItemConfiguration : IEntityTypeConfiguration<MediaItem>
+{
+    public void Configure(EntityTypeBuilder<MediaItem> builder)
+    {
+        builder.HasKey(e => e.Id);
+        builder.Property(e => e.Id).HasConversion(e => e.Value, value => new(value));
+
+        builder.Property(e => e.Url).IsRequired();
+
+        builder.Property(e => e.Type).IsRequired();
+    }
+}
