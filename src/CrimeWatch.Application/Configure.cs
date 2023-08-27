68796055e67bf3963185ff7ace050a3de699bc9b
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-08-28 05:05:20 +0530

    Initialized Application layer

---
+++ src/CrimeWatch.Application/Configure.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace CrimeWatch.Application;
+public static class Configure
+{
+    public static void AddApplication(this IServiceCollection services)
+    {
+    }
+}
