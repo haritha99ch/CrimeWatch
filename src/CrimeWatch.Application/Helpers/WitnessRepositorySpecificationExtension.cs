9c902d06d43e08885f95b649f85b2ae0eccdb87c
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-08-31 03:34:19 +0530

    Implemented User queries testing

--- src/CrimeWatch.Application/Helpers/WitnessRepositorySpecificationExtension.cs
+++ src/CrimeWatch.Application/Helpers/WitnessRepositorySpecificationExtension.cs
@@ -7,6 +7,5 @@ internal static class WitnessRepositorySpecificationExtension
         => await witnessRepository.GetByAsync<WitnessWithAll>(new(witnessId), cancellationToken);
 
     public static async Task<Witness?> GetWitnessWithAllByAccountIdAsync(this IRepository<Witness, WitnessId> witnessRepository, AccountId accountId, CancellationToken cancellationToken)
-       => await witnessRepository.GetByAsync<WitnessWithAll>(new(accountId), cancellationToken);
-}
+        => await witnessRepository.GetByAsync<WitnessWithAll>(new(accountId), cancellationToken);
 }
