37acce524099efe2eca229fac793b903c9966b50
induwara <induwarakanchana567@gmail.com>
2023-09-10 13:11:40 +0530

    Home and About us done

--- src/crimewatch.web.client/src/App.tsx
+++ src/crimewatch.web.client/src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+import About from './Components/About'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Home/>
+      <About/>
     </div>
   )
 }
