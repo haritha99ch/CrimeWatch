7798366f6e52e0f2fa2650b500a96ba069ffa4c5
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-09-12 03:08:15 +0530

    Implemented witness and moderator controllers

---
+++ src/CrimeWatch.Web.API/Controllers/ModeratorController.cs
@@ -0,0 +1,53 @@
+﻿using CrimeWatch.Application.Commands.ModeratorCommands.DeleteModerator;
+using CrimeWatch.Application.Commands.ModeratorCommands.UpdateModerator;
+using CrimeWatch.Application.Queries.ModeratorQueries.GetModerator;
+using CrimeWatch.Application.Queries.ReportQueries.GetReports;
+
+namespace CrimeWatch.Web.API.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class ModeratorController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public ModeratorController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("Get/{moderatorId}")]
+    public async Task<ActionResult<Moderator>> Get([FromRoute] Guid moderatorId)
+    {
+        // Authorized
+        GetModeratorByIdQuery query = new(new(moderatorId));
+        var moderator = await _mediator.Send(query);
+        return moderator != null
+            ? Ok(moderator)
+            : NotFound("Moderator Not Found!");
+    }
+
+    [HttpGet("GetReports/{moderatorId}")]
+    public async Task<ActionResult<List<Report>>> GetReports([FromRoute] Guid moderatorId)
+    {
+        // Authorized
+        GetModeratorReportsQuery query = new(new(moderatorId));
+        var reports = await _mediator.Send(query);
+        return Ok(reports);
+    }
+
+    [HttpPatch("Update")]
+    public async Task<ActionResult<Witness>> Update([FromBody] UpdateModeratorCommand command)
+    {
+        // Authorized
+        var moderator = await _mediator.Send(command);
+        return Ok(moderator);
+    }
+
+    [HttpDelete("Delete/{witnessId}")]
+    public async Task<ActionResult<bool>> Delete([FromRoute] Guid moderatorId)
+    {
+        DeleteModeratorCommand command = new(new(moderatorId));
+        var result = await _mediator.Send(command);
+        return result ? Ok(result) : BadRequest("Could not delete");
+    }
+}
