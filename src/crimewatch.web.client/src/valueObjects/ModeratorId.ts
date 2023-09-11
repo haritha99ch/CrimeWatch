796861084e4d2e14342df57294841268a2568599
Haritha Rathnayake <chamararathnayaka@outlook.com>
2023-09-11 19:56:50 +0530

    Created value objects

---
+++ src/crimewatch.web.client/src/valueObjects/ModeratorId.ts
@@ -0,0 +1,8 @@
+﻿import IValueObject from "../contracts/IValueObject";
+
+export default class ModeratorId implements IValueObject {
+    public value: string;
+    constructor(value: string) {
+        this.value = value;
+    }
+}
---
+++ src/crimewatch.web.client/src/valueObjects/UserId.ts
@@ -0,0 +1,8 @@
+﻿import IValueObject from "../contracts/IValueObject";
+
+export default class UserId implements IValueObject {
+    public value: string;
+    constructor(value: string) {
+        this.value = value;
+    }
+}
