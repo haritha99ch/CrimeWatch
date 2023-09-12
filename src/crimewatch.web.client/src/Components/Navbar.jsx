58e67f0a9467206c42258c9f9b37de6d76d90b3c
induwara <induwarakanchana567@gmail.com>
2023-09-12 21:06:46 +0530

    Support Component done

--- src/crimewatch.web.client/src/Components/Navbar.jsx
+++ src/crimewatch.web.client/src/Components/Navbar.jsx
@@ -23,7 +23,7 @@ const handleClick = () => setNav(!nav)
             <button className="border-none bg-transparent text-black mr-4">SignIn</button>
             <button className="px-8 py-3">Signup</button>
         </div>
-        <div className="md:hidden" onClick={handleClick}>
+        <div className="md:hidden mr-4" onClick={handleClick}>
           {!nav ?  <MenuIcon className="w-5" />:<XIcon className="w-5"/>}
      
         </div>
