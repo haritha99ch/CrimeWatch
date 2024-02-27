2067d59f105db122f10fb06bc6051b190f62f33c
Haritha Rathnayake <chamararathnayaka@outlook.com>
2024-02-27 20:39:13 +0530

    Created and tested AddEvidence command

---
+++ src/Application/Features/Reports/Commands/AddEvidence/AddEvidenceCommandHandler.cs
@@ -0,0 +1,37 @@
+﻿using Application.Specifications.Reports;
+
+namespace Application.Features.Reports.Commands.AddEvidence;
+internal sealed class AddEvidenceCommandHandler : ICommandHandler<AddEvidenceCommand, EvidenceDetails>
+{
+    private readonly IRepository<Report, ReportId> _reportRepository;
+
+    public AddEvidenceCommandHandler(IRepository<Report, ReportId> reportRepository)
+    {
+        _reportRepository = reportRepository;
+    }
+
+    public async Task<Result<EvidenceDetails>> Handle(AddEvidenceCommand request, CancellationToken cancellationToken)
+    {
+        var report = await _reportRepository.GetByIdAsTrackingAsync(request.ReportId, cancellationToken);
+        if (report is null) return ReportNotFoundError.Create();
+
+        var evidence = report.AddEvidence(
+            request.AuthorId,
+            request.Caption,
+            request.Description,
+            request.No,
+            request.Street1,
+            request.Street2,
+            request.City,
+            request.Province,
+            request.MediaItems
+        );
+        await _reportRepository.UpdateAsync(report, cancellationToken);
+        var newEvidence = await _reportRepository.GetOneAsync<EvidenceDetailsById, EvidenceDetails>(
+            new(request.ReportId, evidence.Id),
+            cancellationToken);
+
+        if (newEvidence is null) return EvidenceNotFoundError.Create();
+        return newEvidence;
+    }
+}
---
+++ src/Application/Specifications/Reports/EvidenceDetailsById.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Persistence.Common.Specifications;
+
+namespace Application.Specifications.Reports;
+internal record EvidenceDetailsById : Specification<Report, EvidenceDetails>
+{
+    public EvidenceDetailsById(ReportId reportId, EvidenceId evidenceId) : base(r => r.Id.Equals(reportId))
+    {
+        AddInclude(q => q.Include(r => r.Evidences));
+        Select = new(r => r.Evidences
+            .AsQueryable()
+            .Select(EvidenceDetails.GetProjection)
+            .FirstOrDefault()!);
+    }
+}
